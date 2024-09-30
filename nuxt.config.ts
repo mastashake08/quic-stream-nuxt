d940e1e0c7f487596e017eec63d9ebfdf3d8ac7d
Hacker God <jyrone.parker@gmail.com>
2024-09-29 21:00:40 -0400

    baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
+  app: {
+    baseURL: '/quic-stream-nuxt/'
+  },
   vite: {
     server: {
         proxy: {
